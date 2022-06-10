9678813ab9f088784cf4fd01569c4c21cb8fc509
M0nday0001 <andrey.kovpak.01@gmai.com>
2022-06-10 15:43:49 +0300

    js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-const userName = "Andrew ";
-let age = 29;
+const userName = "Andrew "; // взять в скобки "" будет означать тип данных строк
+let age = 29; // просто ввести число тип данный числовой 
+let boo = true; // ти данный правда или ложь
 
 
 
 
-console.log(userName,age);
+console.log(typeof userName, typeof age, typeof boo); //вывести в консоль тип данных ( строки, число, правда или ложь )
+//что загрузить изменения в свой github нужно вести в терминале :
+// git pull origin master 
