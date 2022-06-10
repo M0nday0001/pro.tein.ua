57bdbc191c05b7454aee0450546e1cb6b67e774a
M0nday0001 <andrey.kovpak.01@gmai.com>
2022-06-10 17:49:26 +0300

    js : selector

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const userName = "Andrew "; // взять в скобки "" будет озна
 let age = 29; // просто ввести число тип данный числовой 
 let boo = true; // ти данный правда или ложь
 
-
-
+const header = document.querySelector(".header");
+const navLinks = document.querySelectorAll(".nav_link");
 
 console.log(typeof userName, typeof age, typeof boo); //вывести в консоль тип данных ( строки, число, правда или ложь )
 // проверка для гитхаба
 // еще одна проверка на изменения строки 
 // доп проверка 
+
+console.log(typeof header);
+console.log(navLinks);
